99338333637a1dc479e44a25c6ac17b26ff65aa9
jotlims <joykolawole007@gmail.com>
2023-02-12 17:48:00 +0100

    commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// let getPictures;
+// fetch("https://dog.ceo/api/breed/hound/images")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     getPictures = data.dog;
+//     console.log(getPictures)
+//     let picture_data = "";
+//     getPictures.map((dogs) => {
+//       console.log(dogs);
+//       picture_data += `<div>
+//      <img src= ${dogs} alt="">
+//       <p class="">${dogs}</p>
+//       </div>`;
+//     });
+
+
+//     document.getElementById("dog").innerHTML = picture_data;
+//   })
+
+
+  let getPhotos;
+   fetch ("https://dog.ceo/api/breed/hound/images")
+ 
+  .then ((response) => response.json())
+  .then ((data) => {
+    getPhotos = data.dog;
+    console.log(getPhotos)
+    
+  })
+
+  document.getElementById("dog").innerHTML 
+
